8eeacb2224e8973dfb1aa4ac2ab7c5f12411cdf3
Allyn Treshansky <allyn.treshansky@gmail.com>
2018-10-25 10:35:03 +0100

    return file contents instead of filepath

--- mapping.py
+++ mapping.py
@@ -1,10 +1,18 @@
 #!flask/bin/python
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, send_file
 from flask_expects_json import expects_json
 
+import json
 import os
 import subprocess
 
+
+#############
+# constants #
+#############
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+DATA_DIR = os.path.join(BASE_DIR, "data")
 
 ###########
 # the app #
@@ -52,49 +60,52 @@ def handle_invalid_usage(error):
     return response
 
 ###################
+# some helper fns #
+###################
+
+def setup_file_path(file_path):
+    """
+    Removes the file if it already exists.
+    Otherwise creates the directory if it doesn't already exist.
+    """
+    try:
+        os.remove(file_path)
+    except OSError:
+        os.makedirs(os.path.dirname(file_path), exist_ok=True)
+
+###################
 # routing / views #
 ###################
 
 @expects_json(schema)
 @app.route('/api/v1/tippecanoe', methods=['POST'])
 def tippecanoe_request():
-
-    print("one")    
     
     basename = request.json["basename"]
     geojson_data = request.json["geojson"]
 
-    print("two")    
-
     geojson_file_path = os.path.join(
-        os.path.dirname(__file__),
-        "data",
+        DATA_DIR,
         basename + ".geojson",
     )
-    os.makedirs(os.path.dirname(geojson_file_path), exist_ok=True)
-
-    print("three")    
+    setup_file_path(geojson_file_path)
 
     mbtiles_file_path = os.path.join(
-        os.path.dirname(__file__),
-        "data",
+        DATA_DIR,
         basename + ".mbitles"
     )
-    os.makedirs(os.path.dirname(mbtiles_file_path), exist_ok=True)
-
-    print("four")    
+    setup_file_path(mbtiles_file_path)
 
     with open(geojson_file_path, "w+") as fp:
         json.dump(geojson_data, fp)
 
-    print("five")    
+    try:
+        cmd = f"tippecanoe -o {mbtiles_file_path} -zg --drop-densest-as-needed {geojson_file_path}"
+        proc = subprocess.run(cmd.split())
+    except Exception as e:
+        raise InvalidUsage(e.message)
 
-    cmd = f"tippecanoe -o {mbtiles_file_path} -zg --drop-densest-as-needed {geojson_file_path}"
-    proc = subprocess.run(cmd.split())
-
-    print("six")
-    
-    return "hello world"
+    return send_file(mbtiles_file_path)
 
 #############
 # run flask #
